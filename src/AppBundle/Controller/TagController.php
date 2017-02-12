7a9567bbfc04360a6db137f4ffd702d19406ec03
MVO2015 <mvondr@gmail.com>
2017-02-12 23:42:12 +0100

    Tag entity, api.

---
+++ src/AppBundle/Controller/TagController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+class TagController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/tag/{id}")
+     * @param $id
+     * @return View|null|object
+     */
+    public function idAction($id)
+    {
+        $singleResult = $this->getDoctrine()->getRepository('AppBundle:Tag')->find($id);
+        if ($singleResult === null) {
+            return new View("tag not found", Response::HTTP_NOT_FOUND);
+        }
+        return $singleResult;
+    }
+
+    /**
+     * @Rest\Get("/tag/")
+     * @param Request $request
+     * @return array|View
+     * @internal param $data
+     */
+    public function searchAction(Request $request)
+    {
+        $search = $request->query->get('search');
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Tag');
+        $query = $repo->createQueryBuilder('tag')
+            ->where('tag.tag LIKE :text')
+            ->setParameter('text', $search.'%')
+            ->getQuery();
+        $singleResult = $query->getResult();
+
+        if ($singleResult === null) {
+            return new View("tag not found", Response::HTTP_NOT_FOUND);
+        }
+        return $singleResult;
+    }
+}
