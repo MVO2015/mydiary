e8f012d16c240fa6b483e14b4fb37245d42f7b9d
Martin Vondracek <Martin.Vondracek@lmc.eu>
2017-02-28 16:01:37 +0100

    Show action, new fonts (commit from console).

--- src/AppBundle/Utils/Text.php
+++ src/AppBundle/Utils/Text.php
@@ -20,4 +20,44 @@ class Text
         $shortText = substr($text,0,$pos) . "...";
         return strlen($shortText) < strlen($text) ? $shortText : $text;
     }
+
+    /**
+     * Get the first sentence from string
+     * @param string $content Input string
+     * @return string The first sentence
+     */
+    public function getFirstSentence($content) {
+
+        $string = preg_replace("/[\s]/", " ", $content); // replace whitespaces with spaces
+
+        $dot = strpos($string, '. ');
+        $exc = strpos($string, '! ');
+        $que = strpos($string, '? ');
+
+        $string = trim($string);
+        $len = strlen($string);
+
+        $dot = $dot ? $dot : $len;
+        $exc = $exc ? $exc : $len;
+        $que = $que ? $que : $len;
+
+        $pos = min($dot, $exc, $que);
+        $result = substr($string, 0, $pos + 1);
+        // do not show last dot
+        if (substr($result, - 1, 1) == '.')
+        {
+            $result = substr($result, 0, -1);
+        }
+        return $result;
+    }
+
+    /**
+     * Get headline of an article as first sentence or shorten first sentence
+     * @param $content
+     * @return string
+     */
+    public function getHeadline($content)
+    {
+        return $this->shorten($this->getFirstSentence($content));
+    }
 }
